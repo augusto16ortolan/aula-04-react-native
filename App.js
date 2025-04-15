9569656c998967559b7f7021f973e82b8ed9921f
Augusto <augustoortolan02@gmail.com>
2025-04-15 19:21:41 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Gremio não tem mundial</Text>
+      <Text style={{ fontSize: 50 }}>Gremio não tem mundial</Text>
+      <Text>Gremio não tem mundial</Text>
+      <Button title="Clique aqui" color={"red"} />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>Clique aqui</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#78f274",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
+    color: "white",
+    fontWeight: "bold",
+  },
+  button: {
+    width: 200,
+    height: 70,
+    backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
   },
 });
